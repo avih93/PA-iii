b33fa57e0b41b8c70c23387a76280e85183eccba
Ángel Viveros Hernández <a_ngel@hotmail.com>
2016-03-15 16:20:04 -0600

    Caminata 15 de marzo
    Se convirtio la practica de caminata aleatoria en funcion

---
+++ caminata.R
@@ -0,0 +1,25 @@
+#La funcion de caminata aleatoria
+#EMpezando en x y subiendo o bajando 
+#La funcion recibe cuantos pasos se van a dar 
+#Al final enseña una grafica con el numero de pasos 
+
+caminata <- function(numero,y){
+  v <- vector("numeric",numero)
+  
+  for(i in 1:numero){
+    v[i]<- y
+    moneda <- runif(1)
+    if(moneda <= 0.5){
+      y<- y+1
+    }else{
+      y <- y-1
+    }
+  }
+  v
+  
+}
+
+final <- caminata(10,100)
+plot(final,type = "l")
+
+
